2d9b48b5a563f62d85f176069223c8ae4a4bb2e2
Gil Dekel <stagadish@users.noreply.github.com>
2014-03-13 00:35:55 -0400

    Update Map.java
    (1) I think it is real critical to have the HashMap<Point, ArrayList<Organism>> as a field variable. Each object should keep it for the duration of its existence. (see lines 16; 24; 35; 36)
    
    (2) Creating an Array of Organisms is not needed here. Map does not require an Organism collection of its own.. (line 37)

--- Ecosystem/Map.java
+++ Ecosystem/Map.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 public class Map {
     private int gridWidth = 0;
     private int gridHeight = 0;
+    private HashMap<Point, ArrayList<Organism>> coordinateMap;
     
     /**
     * Default constructor for the 12x12 Map Grid
@@ -20,6 +21,7 @@ public class Map {
     public Map(){
         gridWidth = 12;
         gridHeight = 12;
+        coordinateMap = new HashMap<Point, ArrayList<Organism>>();
     }
     
     /**
@@ -30,12 +32,10 @@ public class Map {
     public Map(int x, int y){
         gridWidth = x;
         gridHeight = y;
+        coordinateMap = new HashMap<Point, ArrayList<Organism>>();
     }
     
-    public void generateGrid(){
-        HashMap<Point,ArrayList<Organism>> coordinateMap = new HashMap<>();
-        ArrayList<Organism> organisms = new ArrayList<>();
-        
+    public void generateGrid() {
         for(int i = 0; i < gridWidth; i++) {
             for(int j = 0; j < gridHeight; j++) {
                 Point p = new Point(i,j);
@@ -44,11 +44,10 @@ public class Map {
                 Double tempX = p.getX();
                 Double tempY = p.getY();
                 System.out.print(tempX.intValue() +","+ tempY.intValue() +" -"+
-                    coordinateMap.get(p).size()+ "\t\t");
+                    coordinateMap.get(p).size()+ "\t");
 
             }
             System.out.println();
-
         }
     }
 }
